91d51e58144b5ea965e6e098dcfb2d2d0647da18
Sam9527-M <rice59678@gmail.com>
2026-04-22 05:10:24 +0800

    更新 Youjia-BY.js

--- Egern/Youjia-BY.js
+++ Egern/Youjia-BY.js
@@ -267,7 +267,7 @@ export default async function (ctx) {
       { type: "spacer", length: 4 },
       {
         type: "text",
-        text: `¥${format(PRICE[key])}元/升`,
+        text: `¥${format(PRICE[key])}`,
         font: { size: 13, weight: "semibold" },
         textColor: THEME.text
       }
